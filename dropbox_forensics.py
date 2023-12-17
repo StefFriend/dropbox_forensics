3d0378e12a13ff53f375b7b5d61e7af9d46f39ef
StefFriend <stefanoantonio.amico@mail.polimi.it>
2023-12-17 23:01:42 +0100

    better logic

--- dropbox_forensics.py
+++ dropbox_forensics.py
@@ -1,24 +1,37 @@
 import requests
 import logging
 import hashlib
+import os
+from datetime import datetime
 
-# Configure logging
-logging.basicConfig(level=logging.DEBUG, filename='http_requests.log')
+def create_timestamped_dir():
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    directory = os.path.join(os.getcwd(), timestamp)
+    os.makedirs(directory, exist_ok=True)
+    return directory
+
+def setup_logging(directory):
+    log_file = os.path.join(directory, 'activity.log')
+    logging.basicConfig(level=logging.DEBUG, filename=log_file, filemode='w')
 
 def change_link_format(url):
     if url.endswith('?dl=0'):
-        return url[:-1] + '1'  # Replace dl=0 with dl=1
+        return url[:-1] + '1'
     return url
 
-def download_file(url):
+def download_file(url, directory):
     try:
+        local_filename = url.split('/')[-1].split('?')[0]
+        if not local_filename.endswith('.zip'):
+            local_filename += '.zip'
+        local_filepath = os.path.join(directory, local_filename)
+        
         with requests.get(url, stream=True) as response:
             response.raise_for_status()
-            local_filename = url.split('/')[-1].split('?')[0]
-            with open(local_filename, 'wb') as file:
+            with open(local_filepath, 'wb') as file:
                 for chunk in response.iter_content(chunk_size=8192): 
                     file.write(chunk)
-            return local_filename
+            return local_filepath
     except requests.RequestException as e:
         logging.error(f"Error downloading file: {e}")
         return None
@@ -34,11 +47,16 @@ def compute_hash(file_path):
         logging.error(f"Error reading file for hashing: {e}")
         return None
 
-# Example usage
-dropbox_link = 'https://www.dropbox.com/s/example?dl=0'  # Replace with actual Dropbox link
-download_link = change_link_format(dropbox_link)
+def main():
+    directory = create_timestamped_dir()
+    setup_logging(directory)
+    dropbox_link = input("Enter the Dropbox link: ")
+    download_link = change_link_format(dropbox_link)
 
-downloaded_file = download_file(download_link)
-if downloaded_file:
-    file_hash = compute_hash(downloaded_file)
-    logging.info(f"Downloaded file hash (SHA-256): {file_hash}")
+    downloaded_file = download_file(download_link, directory)
+    if downloaded_file:
+        file_hash = compute_hash(downloaded_file)
+        logging.info(f"Downloaded file hash (SHA-256): {file_hash}")
+
+if __name__ == "__main__":
+    main()
